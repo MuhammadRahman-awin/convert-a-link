3186c54481b5d5264df83175c38e1f8ca09d7e5b
Muhammad Rahman <muhammad.rahman@affiliatewindow.com>
2015-11-17 10:18:08 +0000

    using full php tag for compatibility

--- convert-a-link-admin.php
+++ convert-a-link-admin.php
@@ -22,13 +22,13 @@ if ( ! empty( $_POST ) && check_admin_referer( 'insert_publisher' ) ) {
                 <br/>
                 <i>under 'Links and Tools' menu</i>
             </td>
-            <td><a target="_blank" href="http://wiki.affiliatewindow.com/index.php/Convert-a-link"><img src=<?=plugins_url("convert-a-link/one.png")?>></a></td>
+            <td><a target="_blank" href="http://wiki.affiliatewindow.com/index.php/Convert-a-link"><img src=<?php echo plugins_url("convert-a-link/one.png") ?>></a></td>
         </tr>
         <tr>
             <td><b>Step 2: Enter your publisherId and save.</b></td>
             <td>
                 <form enctype="multipart/form-data" name="convert-a-link" method="post" action="">
-                  <?=wp_nonce_field( 'insert_publisher');?>
+                  <?php echo wp_nonce_field( 'insert_publisher'); ?>
                 <input type="number" value="<?php echo esc_attr(get_option('cal_publisherId'))?>" name="publisherId"/>
                     <input type="submit" name="submit" id="submit" class="button button-primary" value="Save">
                 </form>
